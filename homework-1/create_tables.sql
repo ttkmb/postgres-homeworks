d83b3b22eeee493863735fb5a75716a8ef1f3fe3
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2023-10-03 13:44:23 +0300

    complete first work

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+create table employees
+(
+	employee_id int primary key,
+	first_name varchar(25) not null,
+	last_name varchar(25) not null,
+	title varchar(100) not null,
+	birth_date date,
+	notes text
+);
+
+create table customers
+(
+	customer_id varchar(5) primary key,
+	company_name text,
+	contact_name varchar(100)
+);
+
+create table orders
+(
+	order_id int primary key,
+	customer_id varchar(5) references customers(customer_id),
+	employee_id int references employees(employee_id),
+	order_date date,
+	ship_city varchar(55)
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,24 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="fegterpgoou044")
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open('north_data/customers_data.csv', 'r', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                next(reader)
+                cur.executemany("insert into customers values (%s, %s, %s)", reader)
+
+            with open('north_data/employees_data.csv', 'r', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                next(reader)
+                cur.executemany("insert into employees values (%s, %s, %s, %s, %s, %s)", reader)
+
+            with open('north_data/orders_data.csv', 'r', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                next(reader)
+                cur.executemany("insert into orders values (%s, %s, %s, %s, %s)", reader)
+finally:
+    conn.close()
